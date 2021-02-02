71c35359120eeabb4728d984969e5286eac4aa47
Pranav Baburaj <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-02 23:22:47 +0530

    added some test functions

--- testfiles/testfile.js
+++ testfiles/testfile.js
@@ -1,7 +1,16 @@
 
+// this is a tesfile
+// completely for the purpose of
+// testing
+// the package
 
-function hello(){
-    console.log("hello")
+// this is commited to
+// github
+// inorder to get a remote raw file
+// and demonstrate the working of the package
+
+function hello(x){
+    console.log(x * (x * 2))
 }
 
 module.exports = hello;
