d232d95ba0d0a794a1d8214525b801407c4fff20
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-03 09:57:36 +0530

    Corrected the setu.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="lol", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="P Pranav Baburaj",
+    author_email="code-roller@googlegroups.com",
+    description="A simple json database",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/pranavbaburaj/lol-db",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
