56f610e27d65d3189c9ac6e8c52f2027a14d0c74
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-14 19:59:19 +0530

    Planning the cli

--- cli/cli.py
+++ cli/cli.py
@@ -1,7 +1,14 @@
 import click
 
+# all databases created
+DATABASES = []
+
+# database is activated or not
+ACTIVATED_DATABASE = True
+
 @click.command()
 @click.argument('command', type=str)
 @click.argument('name', type=str)
 def main(command, name):
-    print(f"{command} {name}")
+    if command == "create":
+        pass
