1446723ccc033f51d7281de01c427f408ea930fb
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-17 12:17:07 +0530

    formatting

--- project/project.py
+++ project/project.py
@@ -2,6 +2,7 @@ from lol.prompt import Prompt
 from clint.textui import colored as Color
 import os as os
 
+
 # the project class
 class Project:
     def __init__(self):
@@ -13,27 +14,21 @@ class Project:
     def create_prompts(self):
         prompt_solutions = {}
         prompt_queries = [
-            "Project Name", 
+            "Project Name",
             "License",
         ]
 
         for index, el in enumerate(prompt_queries):
             prompt = Prompt(el, typeof=str)
-            prompt_solutions[
-                str(el).replace(" ", "-").lower()
-            ] = prompt.prompt().replace(" ", "_")
+            prompt_solutions[str(el).replace(
+                " ", "-").lower()] = prompt.prompt().replace(" ", "_")
 
         if prompt_solutions['project-name'] == ".":
-            prompt_solutions['project-name'] = os.path.basename(
-                os.getcwd()
-            )
+            prompt_solutions['project-name'] = os.path.basename(os.getcwd())
             prompt_solutions['location'] = os.getcwd()
         else:
             prompt_solutions['location'] = os.path.join(
-                os.getcwd(), prompt_solutions[
-                    'project-name'
-                ]
-            )
+                os.getcwd(), prompt_solutions['project-name'])
 
         return prompt_solutions
 
