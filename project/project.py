e7403c435f7ffc00f0034cf5d752b7c3ed93f82a
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-17 12:16:42 +0530

    Basic functioning

--- project/project.py
+++ project/project.py
@@ -1,11 +1,45 @@
-import click as CommandArgumentParser
 from lol.prompt import Prompt
+from clint.textui import colored as Color
+import os as os
+
+# the project class
+class Project:
+    def __init__(self):
+        self.prompt_message = self.create_prompts()
+
+        print(self.prompt_message)
+
+    # create all the required prompts
+    def create_prompts(self):
+        prompt_solutions = {}
+        prompt_queries = [
+            "Project Name", 
+            "License",
+        ]
+
+        for index, el in enumerate(prompt_queries):
+            prompt = Prompt(el, typeof=str)
+            prompt_solutions[
+                str(el).replace(" ", "-").lower()
+            ] = prompt.prompt().replace(" ", "_")
+
+        if prompt_solutions['project-name'] == ".":
+            prompt_solutions['project-name'] = os.path.basename(
+                os.getcwd()
+            )
+            prompt_solutions['location'] = os.getcwd()
+        else:
+            prompt_solutions['location'] = os.path.join(
+                os.getcwd(), prompt_solutions[
+                    'project-name'
+                ]
+            )
+
+        return prompt_solutions
 
 
-@CommandArgumentParser.command()
-@CommandArgumentParser.argument('command', type=str)
-def main(command):
-    print("Under work")
+def main():
+    project = Project()
 
 
 if __name__ == "__main__":
