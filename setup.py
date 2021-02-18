51633564beb508a51b6e0bf8fff183863a0e37de
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-18 11:30:45 +0530

    New release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ DEPENDENCIES = ['dicttoxml', "clint", "click"]
 
 setuptools.setup(
     name="loldb",  # Replace with your own username
-    version="0.1.2",
+    version="0.1.3",
     author="P Pranav Baburaj",
     author_email="code-roller@googlegroups.com",
     description="A simple json database and other utilities",
