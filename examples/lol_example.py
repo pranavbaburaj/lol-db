435e423171d85b6419f4b0270e25bcbb56e04963
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-21 12:38:37 +0530

    Added the prompt(lol.prompt.Prompt)

--- examples/lol_example.py
+++ examples/lol_example.py
@@ -1,3 +1,10 @@
 from lol.datatype.arrays import Array
 from lol.database.database import Database
 
+from lol.prompt import Prompt
+
+def main():
+    query = ["Org Name"]
+
+if __name__ == "__main__":
+    main()
