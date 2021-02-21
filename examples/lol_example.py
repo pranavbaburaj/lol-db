16240dd71ea384b07e993b1e3fd99eb50b95d51d
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-21 12:43:20 +0530

    Created an example

--- examples/lol_example.py
+++ examples/lol_example.py
@@ -3,12 +3,19 @@ from lol.database.database import Database
 
 from lol.prompt import Prompt
 
+org = Database("name", "members")
+
+
 def main():
     query = ["Org Name", "Members separated by spaces"]
+    sol = {}
     for element in query:
         prompt = Prompt(query)
+        sol[element] = prompt.prompt()
     
-    members = Array.create(str(members).split(" "))
+    members = Array.create(str(sol["Members separated by spaces"]).split(" "))
+
+    org.add([sol["Org Name"], members)
 
 if __name__ == "__main__":
     main()
