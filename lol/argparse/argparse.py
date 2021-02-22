067de6bdfe380a9eacaca3357319976de8e93adf
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-22 12:47:54 +0530

    Completed paramter verification

--- lol/argparse/argparse.py
+++ lol/argparse/argparse.py
@@ -3,7 +3,7 @@ import sys
 def remove_spaces(remove_space_array):
     element_array = []
     for element in remove_space_array:
-        if element is not " ":
+        if element is not "":
             element_array.append(element)
 
     return element_array
@@ -22,6 +22,28 @@ class Parser:
                 element["value"] = remove_spaces(
                     element["value"].split(" ")
                 )
-                print(element)
+                variables = list(filter(
+                    lambda val : val.startswith("$"),
+                    element["value"]
+                ))
+
+                if not "types" in element:
+                    element["types"] = {}
+                else:
+                    assert isinstance(element["types"], dict), "Expected the types to be a dict"
+                
+                for index, item in enumerate(element["value"]):
+                    if item in variables:
+                        if item[1:] in element["types"]:
+                            element["value"][index] = {
+                                "value" : item[1:],
+                                "type" : element["types"][item[1:]]
+                            }
+                        else:
+                            element["value"][index] = {
+                                "value" : item[1:],
+                                "type" : str
+                            }
+                assert "func" in element, "Cannot find key func"
             else:
                 raise TypeError("Expected a dict")
